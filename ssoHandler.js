8514d85098f24915c9619d3e8311a92879808511
José Cardona Figueroa <cardona5d@gmail.com>
2020-07-28 16:50:16 -0500

    Update ssoHandler.js

--- ssoHandler.js
+++ ssoHandler.js
@@ -220,6 +220,7 @@ function SSOHandler(
           alert('finished');
           debugger;
           console.log(result)});
+        Nextiva.SoftPhoneDialController.saveUserData('jose.cardona@nextiva.com', 'lsjñdf', nextiva_token, function(result, event) {alert('finished');debugger;console.log(result);});
         debugger;
         showDialer(true);
       },
